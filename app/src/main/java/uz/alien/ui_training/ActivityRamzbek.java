c2f4a036bdf13724b136480276c6aa8a967cd200
Ramzbek <ramizkarimov002@gmial.com>
2023-10-08 21:10:20 +0500

    изменил

--- app/src/main/java/uz/alien/ui_training/ActivityRamzbek.java
+++ app/src/main/java/uz/alien/ui_training/ActivityRamzbek.java
@@ -1,8 +1,20 @@
 package uz.alien.ui_training;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import uz.alien.ui_training.databinding.ActivityAlienBinding;
+import uz.alien.ui_training.databinding.ActivityRamzbekBinding;
 
 public class ActivityRamzbek extends AppCompatActivity {
 
-    //
+    ActivityRamzbekBinding binding ;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityRamzbekBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
 }
