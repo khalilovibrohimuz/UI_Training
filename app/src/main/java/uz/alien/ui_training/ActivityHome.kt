ebb57f153e161589512469a3014cfefd8e051e22
aliendevuz <aliendevuz@gmail.com>
2023-10-08 20:50:38 +0500

    boshlang'ich holatga keltirildi

--- app/src/main/java/uz/alien/ui_training/ActivityHome.kt
+++ app/src/main/java/uz/alien/ui_training/ActivityHome.kt
@@ -3,16 +3,16 @@ package uz.alien.ui_training
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import uz.alien.ui_training.databinding.ActivityMainBinding
+import uz.alien.ui_training.databinding.ActivityHomeBinding
 
 class ActivityHome : AppCompatActivity() {
 
     private val context = this
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.bAlien.setOnClickListener { startActivity(Intent(context, ActivityAlien::class.java)) }
